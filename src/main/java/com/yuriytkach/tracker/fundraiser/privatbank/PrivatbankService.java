c0b0e34e7b3a2658dc2769d44d635cf9000da913
Yuriy Tkach <yuriytkach@gmail.com>
2023-03-08 01:47:12 +0200

    feat: Add privatbank controller for event
    Only controller and test for now

---
+++ src/main/java/com/yuriytkach/tracker/fundraiser/privatbank/PrivatbankService.java
@@ -0,0 +1,11 @@
+package com.yuriytkach.tracker.fundraiser.privatbank;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PrivatbankService {
+
+  public void syncData() {
+    // nothing to do for now
+  }
+}
---
+++ src/test/java/com/yuriytkach/tracker/fundraiser/integration/PrivatEventTest.java
@@ -0,0 +1,43 @@
+package com.yuriytkach.tracker.fundraiser.integration;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.verify;
+
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.jupiter.api.Test;
+
+import com.yuriytkach.tracker.fundraiser.privatbank.PrivatbankService;
+
+import io.quarkus.amazon.lambda.http.model.AwsProxyRequest;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@QuarkusTest
+class PrivatEventTest implements AwsLambdaTestCommon {
+
+  @InjectMock
+  PrivatbankService privatbankService;
+  @Test
+  void shouldGetTransactionsFromPrivat() {
+    final AwsProxyRequest request = createAwsProxyRequest("/privat/event", "POST", Map.of());
+
+    given()
+      .contentType(MediaType.APPLICATION_JSON)
+      .accept(MediaType.APPLICATION_JSON)
+      .body(request)
+      .when()
+      .post(LAMBDA_URL_PATH)
+      .then()
+      .statusCode(200)
+      .body("statusCode", equalTo(200));
+
+    verify(privatbankService).syncData();
+  }
+
+}
