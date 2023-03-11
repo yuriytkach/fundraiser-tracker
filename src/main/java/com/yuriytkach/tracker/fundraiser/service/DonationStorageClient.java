6ea70a446a0f50c212370e0e0c118fe9573c7e6f
Yuriy Tkach <yuriytkach@gmail.com>
2023-03-11 15:05:31 +0200

    feat: Save multiple donations in one request
    Change donation storage client to save multiple donations using single
    batch write item request to dynamo db.
    
    This will be used latest when processing multiple donations.

--- src/main/java/com/yuriytkach/tracker/fundraiser/service/DonationStorageClient.java
+++ src/main/java/com/yuriytkach/tracker/fundraiser/service/DonationStorageClient.java
@@ -1,15 +1,12 @@
 package com.yuriytkach.tracker.fundraiser.service;
 
 import java.util.Collection;
-import java.util.Optional;
-import java.util.UUID;
 
 import com.yuriytkach.tracker.fundraiser.model.Donation;
 
 public interface DonationStorageClient {
-  void add(String fundId, Donation donation);
+  void addAll(String id, Collection<Donation> donations);
 
   Collection<Donation> findAll(String fundId);
 
-  Optional<Donation> getById(String fundId, UUID id);
 }
